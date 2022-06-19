49da73ca7ca69bcc28f26766c33db53ac8a7c252
fidanmardanli <fidannm@code.edu.az>
2022-06-19 13:52:57 +0400

    Exercises

---
+++ Program.cs
@@ -0,0 +1,105 @@
+﻿namespace Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Checking if the number is divisible by 3 and 7
+            //int num;
+            //Console.WriteLine("Enter the Number :");
+            //num = int.Parse(Console.ReadLine());
+            //if (num % 3 == 0 && num % 7 == 0)
+            //{
+            //    Console.WriteLine("Entered Number is Divisible by 3 and 7 ");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Entered Number is Not Divisible by 3 and 7");
+            //}
+            //Console.ReadLine();
+
+
+
+
+            //if n and m are even numbers, sum them
+            //int n, m;
+            //Console.WriteLine("Enter the Number :");
+            //n = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the Number :");
+            //m = int.Parse(Console.ReadLine());
+            //if (n % 2 == 0 && m % 2 == 0)
+            //{
+            //    Console.WriteLine(n + m);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("One of them is not even number");
+            //}
+
+
+
+
+
+            //find sum of the odd numbers between N and M
+            //int sum = 0;
+            //Console.Write("Enter N:");
+            //int N = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Enter M:");
+            //int M = Convert.ToInt32(Console.ReadLine());
+
+            //if (N < M)
+            //{
+            //    for (int i = N + 1; i <= M; i++)
+            //    {
+            //        if (i % 2 == 1)
+            //            sum += i;
+            //    }
+
+            //    Console.Write($"Sum of Odd numbers from {N} to {M} is : {sum}");
+            //}
+            //else
+            //{
+            //    Console.Write("Invalid");
+            //}
+            //Console.ReadLine();
+
+
+
+
+
+
+
+            //Find sum of the odd numbers in the array
+            //int[] arr = { 1, 2, 3, 4, 5, 6 };
+            //int odd = 0;
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (i % 2 != 0)
+            //        odd += arr[i];
+            //}
+
+            //Console.WriteLine("Sum of odd numbers in the given array="
+            //                       + odd);
+
+
+
+
+
+
+            //find the count of the even numbers in the array
+            //int[] arr = { 1, 2, 3, 4, 5, 6 };
+            //int even = 0;
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (i % 2 == 0)
+            //        even++;
+            //}
+
+            //Console.WriteLine("Count of even numbers in the given array="
+            //                       + even);
+        }
+    }
+}
